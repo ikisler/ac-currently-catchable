9dc3c94c91e5f315b4dc139530c83aca6e4987df
ikisler <ikisler@csumb.edu>
2021-04-27 19:59:23 -0700

    Get data from Nookipedia directly rather than cached file

--- build/buildFish.js
+++ build/buildFish.js
@@ -1,17 +1,34 @@
-const fs = require('fs');
+const https = require('https');
 const fSchedule = require('./fishSchedule.js');
 const fDetails = require('./fishDetails.js');
 
-fs.readFile('./test.json', 'utf8', (err, data) => {
-    if (err) {
-        console.log("Error -- cannot open file");
-    } else {
+/*
+    Data gets sourced from Nookipedia.  More info:
+    https://nookipedia.com/wiki/Nookipedia:Development
+    https://nookipedia.com/wiki/Special:CargoTables/nh_fish
+    https://nookipedia.com/w/api.php?action=help&modules=cargoquery
+*/
+const url  = 'https://nookipedia.com/w/api.php?action=cargoquery&tables=nh_fish&limit=100&fields=name,shadow_size,n_m1,n_m2,n_m3,n_m4,n_m5,n_m6,n_m7,n_m8,n_m9,n_m10,n_m11,n_m12,n_m1_time,n_m2_time,n_m3_time,n_m4_time,n_m5_time,n_m6_time,n_m7_time,n_m8_time,n_m9_time,n_m10_time,n_m11_time,n_m12_time&format=json';
+
+https.get(url, (res) => {
+    let chunks = [];
+    res.on('data', (d) => {
+        chunks.push(d);
+    }).on('end', function() {
+        let data   = Buffer.concat(chunks);
+
         let fishSchedule = new fSchedule();
         let fishDetails = new fDetails();
 
-        const query = JSON.parse(data);
-        fishes = query.cargoquery;
+        try {
+            const query = JSON.parse(data);
+        } catch (e) {
+            console.log('Problem with data response from server');
+            console.log(e);
+            return;
+        }
 
+        fishes = query.cargoquery;
         fishes.forEach(fish => {
             fishDetails.add(fish.title);
             fishSchedule.add(fish.title);
@@ -19,5 +36,8 @@ fs.readFile('./test.json', 'utf8', (err, data) => {
 
         fishSchedule.write();
         fishDetails.write();
-    }
+    });
+}).on('error', (e) => {
+    console.log('Problem with data request...');
+    console.error(e);
 });
