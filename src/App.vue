2b719093712794ecd60c74db4ae81146df93d648
ikisler <ikisler@csumb.edu>
2021-05-04 22:25:54 -0700

    Start working on scrolling through hours
    Change schedule data to be computed, the hour changes mostly work now.  Needs prettying up and unit tests

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
     <header id="title">
         <h1>Animal Crossing New Horizons Currently Catchable</h1>
     </header>
+    <button @click="prevHour()">prev</button>
+    <button @click="nextHour()">next</button>
     <DateHeaderContainer :date="currentDate" :time="currentTime" />
-    <ScheduleContainer v-bind:hours="hours" v-bind:month="month" :allFish="allFish" :fishSchedule="fishSchedule" :fishImageMap="fishImageMap" />
+    <ScheduleContainer :hours="hours" :month="month" :allFish="allFish" :fishSchedule="fishSchedule" :fishImageMap="fishImageMap" />
 </template>
 
 <script>
@@ -20,27 +22,58 @@ export default {
     DateHeaderContainer
     },
     data() {
-        let now = new Date();
-        let dateFormat = new Intl.DateTimeFormat('en-US', { month: 'long', day: 'numeric'});
-        let dateString = dateFormat.format(now);
-        let timeFormat = new Intl.DateTimeFormat('en-US', { hour: 'numeric', minute: 'numeric' });
-        let timeString = timeFormat.format(now);
-        let currentHour = timeFormat.formatToParts(now);
-        now.setHours(now.getHours() + 1);
-        let nextHour = timeFormat.formatToParts(now);
-        now.setHours(now.getHours() + 1);
-        let nextNextHour = timeFormat.formatToParts(now);
-        let hours = [currentHour, nextHour, nextNextHour];
-        let month = now.getMonth();
+        this.start = new Date();
+        this.updateUI();
 
         return {
-            currentDate: dateString,
-            currentTime: timeString,
-            hours: hours,
-            month: month,
+            currentDate: this.currentDate,
+            currentTime: this.currentTime,
+            hours: this.hours,
+            month: this.month,
             allFish: allFish,
             fishSchedule: fishSchedule,
-            fishImageMap: fishImageMap
+            fishImageMap: fishImageMap,
+            start: this.start,
+            end: this.end
+        }
+    },
+    methods: {
+        nextHour() {
+            this.start.setDate(this.end.getDate());
+            this.start.setMonth(this.end.getMonth());
+            this.start.setHours(this.end.getHours() + 1);
+            this.updateUI();
+        },
+        prevHour() {
+            this.start.setHours(this.start.getHours() - 3);
+            this.updateUI();
+        },
+        updateUI() {
+            this.setHours();
+            this.setDisplayDate();
+            this.setDisplayTime();
+        },
+        setHours() {
+            let now = new Date(this.start.getTime());
+            this.month = now.getMonth();
+            let timeFormat = new Intl.DateTimeFormat('en-US', { hour: 'numeric', minute: 'numeric' });
+            let currentHour = timeFormat.formatToParts(now);
+            now.setHours(now.getHours() + 1);
+            let nextHour = timeFormat.formatToParts(now);
+            now.setHours(now.getHours() + 1);
+            let nextNextHour = timeFormat.formatToParts(now);
+            this.hours = [currentHour, nextHour, nextNextHour];
+            this.end = now;
+        },
+        setDisplayDate() {
+            let dateFormat = new Intl.DateTimeFormat('en-US', { month: 'long', day: 'numeric'});
+            this.currentDate = dateFormat.format(this.start);
+        },
+        setDisplayTime() {
+            let timeFormat = new Intl.DateTimeFormat('en-US', { hour: 'numeric'});
+            let startTime = timeFormat.format(this.start);
+            let endTime = timeFormat.format(this.end);
+            this.currentTime = startTime + ' - ' + endTime;
         }
     }
 }
